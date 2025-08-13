e6d5ea782acb083f7852bb441b8b410919a1c539
Joel Hanson <joelhanson025@gmail.com>
2025-08-13 11:31:18 +0530

    feat: sentry added

--- src/middleware.ts
+++ src/middleware.ts
@@ -16,11 +16,11 @@ export default withAuth(
         // Content Security Policy
         const cspDirectives = [
             "default-src 'self'",
-            "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://accounts.google.com",
+            "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://accounts.google.com https://js.sentry-cdn.com",
             "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
             "font-src 'self' https://fonts.gstatic.com",
             "img-src 'self' data: https: blob:",
-            "connect-src 'self' https://api.github.com https://accounts.google.com https://*.googleapis.com",
+            "connect-src 'self' https://api.github.com https://accounts.google.com https://*.googleapis.com https://*.ingest.sentry.io https://*.ingest.de.sentry.io",
             "frame-src 'self' https://accounts.google.com",
             "object-src 'none'",
             "base-uri 'self'"
