f95137763cbc1fcb01e836e756dfecaf3634c9ca
Joel Hanson <joelhanson025@gmail.com>
2025-08-19 21:27:30 +0530

    feat: login is still flaky

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production" || process.env.VERCEL_ENV === "production",
+  },
   images: {
     remotePatterns: [new URL('https://lh3.googleusercontent.com/**')],
   },
